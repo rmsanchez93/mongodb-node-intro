7a36548217a52a0dc3b0aa72e11210f266865916
Raul Sanchez <raulsg93@gmail.com>
2019-10-31 09:33:33 -0500

    CRUD functionality through mongoose

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const Teams = require('./models/teams')
+const Players = require('./models/players')
+const PORT = 3000
+
+//use bodyParser
+app.use(bodyParser.json())
+
+
+//connect mongoDB
+mongoose.connect('mongodb://localhost:27017/baseballteams', {useNewUrlParser: true})
+.then(()=> console.log('Mangos connected'));
+
+
+// @TEAMS get all
+app.get('/teams', (req,res)=>{
+    // res.send('This is a GET request')
+    Teams.find()
+    .then(data=> res.json(data))
+    .catch(err => console.log(err));
+    // console.log('GETTING ', allTeams)
+    
+})
+// @TEAMS create one
+app.post('/teams/new', (req,res)=>{
+    Teams.create(req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.json(err));
+
+})
+// @TEAMS get one
+app.get('/teams/:id', (req,res)=>{
+    let {id} = req.params
+    console.log('GETTING ONE', id)
+    Teams.find({_id: id})
+    .then(data=> res.json(data))
+    .catch(err=> res.json(err))
+    // res.send('This is a GET request for one particular item ')
+})
+
+// @TEAMS update one
+app.patch('/teams/:id', async (req, res)=>{
+    let {id} = req.params
+    Teams.findOneAndUpdate(
+        { _id: id },
+        req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.json(err))
+
+})
+
+
+app.delete('/:id', (req, res)=>{
+    Teams.remove(
+        {_id: req.params.id}
+        )
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.json(err))
+})
+
+app.get('/players', (req, res)=>{
+    Players.find()
+    .then(data=> res.json(data))
+    .catch(err => console.log(err));
+})
+app.post('/players', (req, res)=>{
+    Players.create(req.body)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.json(err));
+
+})
+
+
+app.listen(PORT, ()=>console.log(`listening on ${PORT}!`));
